02057653a5aa2a455cd258a4b2ecd18f08a84075
Vedzkun <underdevelopment12@gmail.com>
2025-05-06 23:04:14 +0530

    Creating workspace

---
+++ saas/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Saas",
+  description: "Saas Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
