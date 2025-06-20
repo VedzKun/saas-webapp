786bc55e41308972dd0177875b3a5437f4730bd8
Vedzkun <underdevelopment12@gmail.com>
2025-06-20 11:43:14 +0530

    Adding Components

--- saas/app/layout.tsx
+++ saas/app/layout.tsx
@@ -3,11 +3,13 @@ import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
 
 const fontSans = FontSans({
+  variable: "--font-sans",
   subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-  title: "Saas",
+  title: "hi",
   description: "Saas Project",
 };
 
@@ -18,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${fontSans.variable} antialiased`}>
         {children}
       </body>
     </html>
